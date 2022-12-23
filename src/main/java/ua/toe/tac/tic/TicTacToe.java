493a49ac5691453af2ade52a9a4223b4341d4f3e
dr-Nata <nberkunsky@gmail.com>
2022-12-23 19:25:09 +0200

    Add countY

--- src/main/java/ua/toe/tac/tic/TicTacToe.java
+++ src/main/java/ua/toe/tac/tic/TicTacToe.java
@@ -14,6 +14,7 @@ import static ua.toe.tac.tic.enums.Direction.*;
 
 public class TicTacToe {
     private int countX = 15;
+    private int countY = 25;
     private int coincidenceCount = 3;
     private final Board board;
 
